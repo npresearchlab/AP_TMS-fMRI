9755d305310f3367845d167395e67fa3dd229a9d
Ashley Pelton <apelton3@gatech.edu>
2024-10-29 14:33:20 -0400

    Updated script

--- Nav_Preprocessing/Nav_SPM_Preproc_job.m
+++ Nav_Preprocessing/Nav_SPM_Preproc_job.m
@@ -8,7 +8,8 @@
 %
 %
 % For Ashley <3
-% sub- and spm12 folder: /Users/nprluser/Documents/AP/Nav_Preproc
+% sub- folders location: /Users/nprluser/Documents/AP/Nav_Subjects
+% spm12 folder location: /Users/nprluser/Documents/AP/AP_TMS-fMRI
 % dcm2niix location:/Library/Frameworks/PythonVersions/3.12/bin/dcm2niix
 % Slice Number = 72
 % TR = 1.5 s
